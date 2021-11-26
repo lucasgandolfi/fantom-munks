6f0abde5c15ee00d9b0148bd788c3fd83ad7fc50
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-11-26 17:10:03 -0300

    feat: page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Fantom Munks</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
